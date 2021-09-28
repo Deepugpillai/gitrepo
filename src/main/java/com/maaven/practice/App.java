bd681f3f95fcc89c2e853cf8e299d4a313f19cac
Deepugpillai <deepu.g.pillai@live.com>
2021-09-29 00:25:23 +0530

    first code

--- src/main/java/com/maaven/practice/App.java
+++ src/main/java/com/maaven/practice/App.java
@@ -1,13 +1,13 @@
 package com.maaven.practice;
 
 /**
- * Hello world!
+ * Hello World,Its me Deepu!
  *
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World,Its me Deepu!" );
     }
 }
